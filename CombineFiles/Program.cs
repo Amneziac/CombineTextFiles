eb21f7d87c63ec25913b4e912f9c04680b929f58
Amneziac <Dmars8047@live.com>
2016-11-12 18:46:16 -0500

    First Commit
    Program that combines text files into one file. Checks for column names
    in the first line(s) of the filse so that it does not repeat if column
    headers are completed in the files.

---
+++ CombineFiles/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CombineFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please enter the path to the directory where files are to be combined: ");
+            var dirPath = Console.ReadLine();
+
+            List<FileInfo> files = new List<FileInfo>();
+
+            try
+            {
+
+                if (Directory.Exists(dirPath))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(dirPath);
+
+                    foreach (var f in dir.GetFiles("*.txt"))
+                    {
+                        files.Add(f);
+                    }
+
+                    Console.WriteLine("\n");
+                    
+                    Console.WriteLine("\nInitiating file merge... ");
+
+                    ConcatenateFiles(files);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void ConcatenateFiles(List<FileInfo> inputFiles)
+        {
+            using (StreamWriter sw = File.CreateText(@"C:\temp\test.txt"))
+            {
+                string firstline = File.ReadLines(inputFiles.First().FullName).First();
+                bool hasWrittenFirstLine = false;
+
+                foreach (var file in inputFiles)
+                {
+                    foreach (var line in File.ReadLines(file.FullName))
+                    {
+                        if (line != firstline)
+                            sw.WriteLine(line);
+                        else if (line == firstline && hasWrittenFirstLine == false)
+                        {
+                            sw.WriteLine(line);
+                            hasWrittenFirstLine = true;
+                        }
+                    }
+                }
+            }
+
+            //using (Stream output = new FileStream(primaryFile.FullName, FileMode.Append))
+            //{
+            //    foreach (var inputFile in inputFiles)
+            //    {
+            //        using (Stream input = File.OpenRead(inputFile.FullName))
+            //        {
+            //            input.CopyTo(output);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
